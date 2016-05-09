9126f5ebf51d36bb5944219e84e365141b188112
Shohei Aoki <shoaok@gmail.com>
2016-05-09 13:37:23 +0900

    extruder number to zero

--- Repetier/Configuration.h
+++ Repetier/Configuration.h
@@ -46,7 +46,7 @@ To override EEPROM settings with config settings, set EEPROM_MODE 0
 // BASIC SETTINGS: select your board type, thermistor type, axis scaling, and endstop configuration
 
 /** Number of extruders. Maximum 6 extruders. */
-#define NUM_EXTRUDER 1
+#define NUM_EXTRUDER 0
 
 /** Set to 1 if all extruder motors go to 1 nozzle that mixes your colors. */
 #define MIXING_EXTRUDER 0
